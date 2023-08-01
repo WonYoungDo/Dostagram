f694ba42fdc796890c86df395d06c7d6fc06024b
DESKTOP-NJKHAHU\user <mile@naver.com>
2023-08-01 21:53:03 +0900

    (원영도) .server port 설정 .hello world 출력

--- src/main/java/com/tawny/dostagram/hello/HelloController.java
+++ src/main/java/com/tawny/dostagram/hello/HelloController.java
@@ -8,7 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 	
 	@GetMapping("/hello")
+	@ResponseBody
 	public String hello() {
+		return "hellow/hello";
+	}
+	
+	@GetMapping("/hello/1")
+	public String hello1() {
 		return "hellow/hello";
 	}
 }
